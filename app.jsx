673da69c687424664f4dd3601fd73aa26fe73087
Oleg Polyakov <olegpolyakov@outlook.com>
2016-09-07 23:19:00 +0300

    JSX

---
+++ app.jsx
@@ -0,0 +1,8 @@
+var jsx = 
+<div className="container">
+    <img src="https://facebook.github.io/react/img/logo.svg" />
+    <h1>React</h1>
+    <p>Библиотека для создания пользовательских интерфейсов</p>
+</div>;
+
+ReactDOM.render(jsx, document.getElementById('root'));
