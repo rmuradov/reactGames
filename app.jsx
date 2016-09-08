49cf7abd18fd3e96fb9d47385382c369080db8f1
Oleg Polyakov <olegpolyakov@outlook.com>
2016-09-08 14:58:39 +0300

    Components

--- app.jsx
+++ app.jsx
@@ -1,8 +1,11 @@
-var jsx = 
-<div className="container">
-    <img src="https://facebook.github.io/react/img/logo.svg" />
-    <h1>React</h1>
-    <p>Библиотека для создания пользовательских интерфейсов</p>
-</div>;
+function Hero() {
+    return (
+        <div className="container">
+            <img src="https://facebook.github.io/react/img/logo.svg" />
+            <h1>React</h1>
+            <p>Библиотека для создания пользовательских интерфейсов</p>
+        </div>
+    );
+}
 
-ReactDOM.render(jsx, document.getElementById('root'));
+ReactDOM.render(<Hero />, document.getElementById('root'));
